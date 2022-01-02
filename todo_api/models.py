85bb930bc444ab5656720ece40a6329f9d346d0d
mukaj <43552866+mukaj@users.noreply.github.com>
2022-01-01 20:05:10 -0500

    Correct Model Fields
    * Renamed "Name" field to "Title"
    * Made Title, Completed, CreatedOn fields to not be allowed to be
    null.

--- todo_api/models.py
+++ todo_api/models.py
@@ -7,10 +7,10 @@ class User(models.Model):
     Name = models.CharField(max_length=50)
 
 class Task(models.Model):
-    Name = models.CharField(max_length=50)
+    Title = models.CharField(max_length=50, null=False)
     Description = models.TextField(max_length=1000)
-    Completed = models.BooleanField()
-    CreatedOn = models.DateTimeField(auto_now_add=True)
+    Completed = models.BooleanField(null=False)
+    CreatedOn = models.DateTimeField(auto_now_add=True, null=False)
     CompletedOn = models.DateTimeField()
     BelongsTo = models.ForeignKey(
         User,
