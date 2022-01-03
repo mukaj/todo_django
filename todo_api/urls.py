96eb5206c32ac1bd46e1b382c6a45461379c6cd2
mukaj <43552866+mukaj@users.noreply.github.com>
2022-01-02 20:11:15 -0500

    Add CRUD for Individual Task
    Added GET, PUT, PATCH, DELETE for tasks by using pk.

--- todo_api/urls.py
+++ todo_api/urls.py
@@ -3,4 +3,5 @@ from todo_api import views
 
 urlpatterns = [
     path('tasks/', views.task_list),
+    path('task/<int:pk>', views.task_detail),
 ]
--- todo_api/views.py
+++ todo_api/views.py
@@ -7,10 +7,14 @@ from rest_framework.parsers import JSONParser
 from todo_api.models import Task
 from todo_api.serializers import TaskSerializer
 
-@csrf_exempt
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET', 'POST'])
 def task_list(request):
     """
-    List all code snippets, or create a new snippet.
+    List all tasks or create a new task
     """
     if request.method == 'GET':
         tasks = Task.objects.all()
@@ -22,5 +26,37 @@ def task_list(request):
         serializer = TaskSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def task_detail(request, pk):
+    """
+    Retrieve, update or delete a task.
+    """
+    try:
+        task = Task.objects.get(pk=pk)
+    except Task.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'PATCH':
+        serializer = TaskSerializer(task, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
